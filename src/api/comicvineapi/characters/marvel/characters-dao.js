18d34713bcb8b5bed58e2783f83ab48aedc5a4fd
Kevin Amiranoff <kamiranoff@gmail.com>
2017-12-29 12:23:28 +0000

    linting and allow localhost:8889

--- src/api/comicvineapi/characters/marvel/characters-dao.js
+++ src/api/comicvineapi/characters/marvel/characters-dao.js
@@ -7,14 +7,16 @@ const fieldsToRetreive = 'character.name character.image character.id character.
 
 comicvineCharacterSchema.statics.getAllNamesAndAppearancesFromMarvel = () => {
   return new Promise((resolve, reject) => {
-    let _query = {'character.publisher.name':'Marvel','character.count_of_issue_appearances':{ $gt: 99}};
+    let _query = { 'character.publisher.name': 'Marvel', 'character.count_of_issue_appearances': { $gt: 99 } };
     let fields = 'character.name character.id character.count_of_issue_appearances';
 
     Characters
-      .find(_query,fields)
+      .find(_query, fields)
       .sort({ "character.count_of_issue_appearances": -1 })
       .exec((err, characters) => {
-        if(err){ reject(err)}else{
+        if (err) {
+          reject(err)
+        } else {
           resolve(characters);
         }
       });
@@ -23,15 +25,17 @@ comicvineCharacterSchema.statics.getAllNamesAndAppearancesFromMarvel = () => {
 
 comicvineCharacterSchema.statics.getAllFromMarvel = () => {
   return new Promise((resolve, reject) => {
-    let _query = {'character.publisher.name':'Marvel'};
+    let _query = { 'character.publisher.name': 'Marvel' };
     let fields = fieldsToRetreive;
 
     Characters
-      .find(_query,fields)
+      .find(_query, fields)
       .sort({ "character.name": 1 })
       .limit(100)
       .exec((err, characters) => {
-        if(err){ reject(err)}else{
+        if (err) {
+          reject(err)
+        } else {
           resolve(characters);
         }
       });
@@ -39,15 +43,13 @@ comicvineCharacterSchema.statics.getAllFromMarvel = () => {
 };
 
 
-
-
-comicvineCharacterSchema.statics.getMoreCharacters = (lastName,qty) => {
+comicvineCharacterSchema.statics.getMoreCharacters = (lastName, qty) => {
   var qty = parseInt(qty);
   return new Promise((resolve, reject) => {
 
     let _query = {
       "character.name": { "$gt": lastName },
-      'character.publisher.name':'Marvel'
+      'character.publisher.name': 'Marvel'
     };
     let fields = fieldsToRetreive;
 
@@ -55,11 +57,13 @@ comicvineCharacterSchema.statics.getMoreCharacters = (lastName,qty) => {
       return reject(new TypeError('is not a valid number.'));
     }
     Characters
-      .find(_query,fields)
+      .find(_query, fields)
       .sort({ "character.name": 1 })
       .limit(qty)
       .exec((err, characters) => {
-        if(err){ reject(err)}else{
+        if (err) {
+          reject(err)
+        } else {
           resolve(characters);
         }
       });
@@ -68,22 +72,22 @@ comicvineCharacterSchema.statics.getMoreCharacters = (lastName,qty) => {
 
 
 comicvineCharacterSchema.statics.getCharactersByName = (input) => {
-  console.log("input",input);
+  console.log("input", input);
   return new Promise((resolve, reject) => {
     if (!_.isString(input)) {
       return reject(new TypeError('is not a valid string.'));
     }
     let _query = {
       "character.name": { "$regex": input, "$options": "i" },
-      'character.publisher.name':'Marvel'
+      'character.publisher.name': 'Marvel'
     };
     let fields = fieldsToRetreive;
     Characters
-      .find( _query ,fields)
+      .find(_query, fields)
       .exec((err, characters) => {
-        if(err){
+        if (err) {
           reject(err)
-        }else{
+        } else {
           resolve(characters);
         }
       });
@@ -92,6 +96,5 @@ comicvineCharacterSchema.statics.getCharactersByName = (input) => {
 };
 
 
-
-const Characters  = mongoose.model('ComicvineCharacters', comicvineCharacterSchema,"charactersdetails");
+const Characters = mongoose.model('ComicvineCharacters', comicvineCharacterSchema, "charactersdetails");
 export default Characters;
